0b4278b8a7822bb3b88d7f050ebf929d2cfa23aa
Ayush Acharya <ayush.acharya96@gmail.com>
2020-02-05 22:09:21 +0545

    Initial Commit

---
+++ src/main/java/com/demoproject/fusecanteen/FusecanteenApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.fusecanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusecanteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FusecanteenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demoproject/fusecanteen/FusecanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.fusecanteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusecanteenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
